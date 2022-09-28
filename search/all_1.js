35a8e9309dbb7efa1f6df8ed5da3af54874816c4
BlopaSc <BlopaSc@users.noreply.github.com>
2022-09-28 08:19:59 +0000

    deploy: c9fa36ead5f0f6a6eba29d96a96a3128888812dd

--- search/all_1.js
+++ search/all_1.js
@@ -4,5 +4,6 @@ var searchData=
   ['begin_1',['begin',['../de/ddf/classbpp_1_1map_1_1_tree_a_v_l.html#aa22230e2bda2d159381f402d108a575c',1,'bpp::map::TreeAVL']]],
   ['bpp_2',['bpp',['../d3/dd4/namespacebpp.html',1,'']]],
   ['bpp_2c_20b_2b_2b_20or_20blopa_2b_2b_3',['BPP, B++ or Blopa++',['../d3/dcc/md__r_e_a_d_m_e.html',1,'']]],
-  ['map_4',['map',['../d0/d27/namespacebpp_1_1map.html',1,'bpp']]]
+  ['list_4',['list',['../d7/da2/namespacebpp_1_1list.html',1,'bpp']]],
+  ['map_5',['map',['../d0/d27/namespacebpp_1_1map.html',1,'bpp']]]
 ];
--- search/namespaces_0.js
+++ search/namespaces_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['bpp_0',['bpp',['../d3/dd4/namespacebpp.html',1,'']]],
-  ['map_1',['map',['../d0/d27/namespacebpp_1_1map.html',1,'bpp']]]
+  ['list_1',['list',['../d7/da2/namespacebpp_1_1list.html',1,'bpp']]],
+  ['map_2',['map',['../d0/d27/namespacebpp_1_1map.html',1,'bpp']]]
 ];
