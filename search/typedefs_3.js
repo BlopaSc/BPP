ff6d2fb61a387ed74a835b163bac499189ec90a3
BlopaSc <BlopaSc@users.noreply.github.com>
2022-12-20 07:17:41 +0000

    deploy: cdcbd46fce7d957bacc737ef6d9b3741e24ad628

--- search/typedefs_3.js
+++ search/typedefs_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['iterator_5fcategory_0',['iterator_category',['../d8/d33/structbpp_1_1collections_1_1map_1_1_tree_a_v_l_1_1iterator.html#a57645fd05945133bb8e7d2d33cc9c6b3',1,'bpp::collections::map::TreeAVL::iterator::iterator_category()'],['../d7/dd0/structbpp_1_1collections_1_1map_1_1_tree_a_v_l_1_1reverse__iterator.html#aeadd8e1ef8ebc1636873604fb7f679a9',1,'bpp::collections::map::TreeAVL::reverse_iterator::iterator_category()']]]
+  ['iterator_5fcategory_0',['iterator_category',['../d8/d82/classbpp_1_1collections_1_1sequence_1_1_combination.html#afd7c853ab26287fa9cd00192e33d3992',1,'bpp::collections::sequence::Combination::iterator_category()'],['../d8/d33/structbpp_1_1collections_1_1map_1_1_tree_a_v_l_1_1iterator.html#a57645fd05945133bb8e7d2d33cc9c6b3',1,'bpp::collections::map::TreeAVL::iterator::iterator_category()'],['../d7/dd0/structbpp_1_1collections_1_1map_1_1_tree_a_v_l_1_1reverse__iterator.html#aeadd8e1ef8ebc1636873604fb7f679a9',1,'bpp::collections::map::TreeAVL::reverse_iterator::iterator_category()']]]
 ];
--- search/typedefs_6.js
+++ search/typedefs_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['pointer_0',['pointer',['../d8/d33/structbpp_1_1collections_1_1map_1_1_tree_a_v_l_1_1iterator.html#a5799078659bf752c75c56700552ec883',1,'bpp::collections::map::TreeAVL::iterator::pointer()'],['../d7/dd0/structbpp_1_1collections_1_1map_1_1_tree_a_v_l_1_1reverse__iterator.html#a88b57c0f11d82f2cf801460c2746310b',1,'bpp::collections::map::TreeAVL::reverse_iterator::pointer()']]]
+  ['pointer_0',['pointer',['../d8/d82/classbpp_1_1collections_1_1sequence_1_1_combination.html#a10d0804a7fd819be2f0e2201e3eb78d2',1,'bpp::collections::sequence::Combination::pointer()'],['../d8/d33/structbpp_1_1collections_1_1map_1_1_tree_a_v_l_1_1iterator.html#a5799078659bf752c75c56700552ec883',1,'bpp::collections::map::TreeAVL::iterator::pointer()'],['../d7/dd0/structbpp_1_1collections_1_1map_1_1_tree_a_v_l_1_1reverse__iterator.html#a88b57c0f11d82f2cf801460c2746310b',1,'bpp::collections::map::TreeAVL::reverse_iterator::pointer()']]]
 ];
