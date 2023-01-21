8b2f29dd30b091778d527991da5579dfb45cfee5
BlopaSc <BlopaSc@users.noreply.github.com>
2023-01-21 08:00:09 +0000

    deploy: b8311c2814b51b692135e3d3aa83a926dd74d9c6

--- search/functions_c.js
+++ search/functions_c.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['rank_0',['rank',['../db/d9b/classbpp_1_1collections_1_1iteration_1_1_combination.html#a1e40bea2630c3904a5676983b8f37c64',1,'bpp::collections::iteration::Combination']]],
   ['rbegin_1',['rbegin',['../d8/dd6/classbpp_1_1collections_1_1map_1_1_tree_a_v_l.html#abdbaac112d33a723d5b44a2a1c9ceede',1,'bpp::collections::map::TreeAVL']]],
-  ['rend_2',['rend',['../db/d9b/classbpp_1_1collections_1_1iteration_1_1_combination.html#a58283a6b3265914f133fc1c7391fdc6e',1,'bpp::collections::iteration::Combination::rend()'],['../d8/dd6/classbpp_1_1collections_1_1map_1_1_tree_a_v_l.html#afa710644dcff03d8197c54732e8d39d0',1,'bpp::collections::map::TreeAVL::rend()']]],
-  ['reverse_5fiterator_3',['reverse_iterator',['../d7/dd0/structbpp_1_1collections_1_1map_1_1_tree_a_v_l_1_1reverse__iterator.html#a57048d268e273e3d2143011dbb96a051',1,'bpp::collections::map::TreeAVL::reverse_iterator']]]
+  ['rend_2',['rend',['../db/d9b/classbpp_1_1collections_1_1iteration_1_1_combination.html#a58283a6b3265914f133fc1c7391fdc6e',1,'bpp::collections::iteration::Combination::rend()'],['../d8/dd6/classbpp_1_1collections_1_1map_1_1_tree_a_v_l.html#afa710644dcff03d8197c54732e8d39d0',1,'bpp::collections::map::TreeAVL::rend()']]]
 ];
