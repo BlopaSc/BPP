c31db0614497c4cdfb9f3cf1325147f9b8c2aa71
BlopaSc <BlopaSc@users.noreply.github.com>
2023-01-22 03:57:44 +0000

    deploy: 8137c4e7b095c1e84c9894f904aec12a24d8345a

--- search/all_1.js
+++ search/all_1.js
@@ -1,11 +1,12 @@
 var searchData=
 [
   ['b_2b_2b_20library_20collection_0',['B++ library collection',['../index.html',1,'']]],
-  ['begin_1',['begin',['../d8/dd6/classbpp_1_1collections_1_1map_1_1_tree_a_v_l.html#a0dbeef9acd3cc84d55ec378315d3cf3e',1,'bpp::collections::map::TreeAVL']]],
-  ['bpp_2',['bpp',['../d3/dd4/namespacebpp.html',1,'']]],
-  ['bpp_2c_20b_2b_2b_20or_20blopa_2b_2b_3',['BPP, B++ or Blopa++',['../d3/dcc/md__r_e_a_d_m_e.html',1,'']]],
-  ['collections_4',['collections',['../d0/d04/namespacebpp_1_1collections.html',1,'bpp']]],
-  ['iteration_5',['iteration',['../dc/d91/namespacebpp_1_1collections_1_1iteration.html',1,'bpp::collections']]],
-  ['map_6',['map',['../d3/d6c/namespacebpp_1_1collections_1_1map.html',1,'bpp::collections']]],
-  ['priority_5fqueue_7',['priority_queue',['../dd/d0e/namespacebpp_1_1collections_1_1priority__queue.html',1,'bpp::collections']]]
+  ['base_1',['base',['../d7/dd0/structbpp_1_1collections_1_1map_1_1_tree_a_v_l_1_1reverse__iterator.html#a48478b3a2d085bb3ddb44d0a9f178c4e',1,'bpp::collections::map::TreeAVL::reverse_iterator::base()'],['../dc/ddd/structbpp_1_1collections_1_1map_1_1_tree_a_v_l_1_1const__reverse__iterator.html#a3bcd18b75efb92a1333634bac4ebf4db',1,'bpp::collections::map::TreeAVL::const_reverse_iterator::base()']]],
+  ['begin_2',['begin',['../d8/dd6/classbpp_1_1collections_1_1map_1_1_tree_a_v_l.html#a0dbeef9acd3cc84d55ec378315d3cf3e',1,'bpp::collections::map::TreeAVL']]],
+  ['bpp_3',['bpp',['../d3/dd4/namespacebpp.html',1,'']]],
+  ['bpp_2c_20b_2b_2b_20or_20blopa_2b_2b_4',['BPP, B++ or Blopa++',['../d3/dcc/md__r_e_a_d_m_e.html',1,'']]],
+  ['collections_5',['collections',['../d0/d04/namespacebpp_1_1collections.html',1,'bpp']]],
+  ['iteration_6',['iteration',['../dc/d91/namespacebpp_1_1collections_1_1iteration.html',1,'bpp::collections']]],
+  ['map_7',['map',['../d3/d6c/namespacebpp_1_1collections_1_1map.html',1,'bpp::collections']]],
+  ['priority_5fqueue_8',['priority_queue',['../dd/d0e/namespacebpp_1_1collections_1_1priority__queue.html',1,'bpp::collections']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['begin_0',['begin',['../d8/dd6/classbpp_1_1collections_1_1map_1_1_tree_a_v_l.html#a0dbeef9acd3cc84d55ec378315d3cf3e',1,'bpp::collections::map::TreeAVL']]]
+  ['base_0',['base',['../d7/dd0/structbpp_1_1collections_1_1map_1_1_tree_a_v_l_1_1reverse__iterator.html#a48478b3a2d085bb3ddb44d0a9f178c4e',1,'bpp::collections::map::TreeAVL::reverse_iterator::base()'],['../dc/ddd/structbpp_1_1collections_1_1map_1_1_tree_a_v_l_1_1const__reverse__iterator.html#a3bcd18b75efb92a1333634bac4ebf4db',1,'bpp::collections::map::TreeAVL::const_reverse_iterator::base()']]],
+  ['begin_1',['begin',['../d8/dd6/classbpp_1_1collections_1_1map_1_1_tree_a_v_l.html#a0dbeef9acd3cc84d55ec378315d3cf3e',1,'bpp::collections::map::TreeAVL']]]
 ];
