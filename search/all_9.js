b56a6c235701b36160909a7614bd125786d45169
BlopaSc <BlopaSc@users.noreply.github.com>
2023-07-23 02:56:55 +0000

    deploy: 37a8154dad9eadad3584b9a5e624869a4075e14e

--- search/all_9.js
+++ search/all_9.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['k_0',['k',['../db/d9b/classbpp_1_1collections_1_1iteration_1_1_combination.html#a776da627af073324932d7f0a1b46d9dd',1,'bpp::collections::iteration::Combination']]],
   ['key_5fcompare_1',['key_compare',['../d8/dd6/classbpp_1_1collections_1_1map_1_1_tree_a_v_l.html#a8e807c0f1bc4abcb016cfc2ed6331cf4',1,'bpp::collections::map::TreeAVL']]],
-  ['key_5ftype_2',['key_type',['../d8/dd6/classbpp_1_1collections_1_1map_1_1_tree_a_v_l.html#a77469c35210f2cd677d8eaed5dde8720',1,'bpp::collections::map::TreeAVL::key_type()'],['../d6/da7/classbpp_1_1collections_1_1map_1_1_trie.html#aeaf067c81afaef20ac90255622d5cdc8',1,'bpp::collections::map::Trie::key_type()']]]
+  ['key_5ftype_2',['key_type',['../d8/dd6/classbpp_1_1collections_1_1map_1_1_tree_a_v_l.html#a171a248b242eb701a7512426434153be',1,'bpp::collections::map::TreeAVL::key_type()'],['../d6/da7/classbpp_1_1collections_1_1map_1_1_trie.html#aeaf067c81afaef20ac90255622d5cdc8',1,'bpp::collections::map::Trie::key_type()']]]
 ];
--- search/typedefs_4.js
+++ search/typedefs_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['key_5fcompare_0',['key_compare',['../d8/dd6/classbpp_1_1collections_1_1map_1_1_tree_a_v_l.html#a8e807c0f1bc4abcb016cfc2ed6331cf4',1,'bpp::collections::map::TreeAVL']]],
-  ['key_5ftype_1',['key_type',['../d8/dd6/classbpp_1_1collections_1_1map_1_1_tree_a_v_l.html#a77469c35210f2cd677d8eaed5dde8720',1,'bpp::collections::map::TreeAVL::key_type()'],['../d6/da7/classbpp_1_1collections_1_1map_1_1_trie.html#aeaf067c81afaef20ac90255622d5cdc8',1,'bpp::collections::map::Trie::key_type()']]]
+  ['key_5ftype_1',['key_type',['../d8/dd6/classbpp_1_1collections_1_1map_1_1_tree_a_v_l.html#a171a248b242eb701a7512426434153be',1,'bpp::collections::map::TreeAVL::key_type()'],['../d6/da7/classbpp_1_1collections_1_1map_1_1_trie.html#aeaf067c81afaef20ac90255622d5cdc8',1,'bpp::collections::map::Trie::key_type()']]]
 ];
